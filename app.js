b38487109174827a0ecbaa3be22b2bdfe5d256cc
Adam Chambers <adammchambers@gmail.com>
2017-08-05 17:26:00 -0400

    Logic to count words and characters on keyup, flexify styles

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var count = require('word-count')
+const count = require('word-count')
+
+const ui = {
+  canvas: document.querySelector('[data-canvas]'),
+  counts: {
+    words: document.querySelector('[data-count="words"]'),
+    chars: document.querySelector('[data-count="chars"]')
+  }
+}
+
+ui.canvas.addEventListener('keyup', () => {
+  ui.counts.words.innerHTML = count(ui.canvas.value)
+  ui.counts.chars.innerHTML = ui.canvas.value.length
+})
